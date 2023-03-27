e3b456b78da609e6aae8fd40cd13114aa6619895
Júlia <julia.sp2005@aluno.ifsc.edu.br>
2023-03-27 09:07:21 -0300

    renomeação da abertura

---
+++ abertura.js
@@ -0,0 +1,18 @@
+export default class abertura extends Phaser.Scene {
+    constructor() {
+        super("abertura");
+    }
+
+    preload() {
+        // Carregamento de arquivos/objetos em memória
+    }
+
+    create() {
+        // Carregar em tela e/ou
+        // Registrar eventos
+    }
+
+    update() {
+        // Código executado a cada frame
+    }
+}
