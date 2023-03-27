4caa02efe0b5f9861ec78ddc72a7c94482d7d860
Júlia <julia.sp2005@aluno.ifsc.edu.br>
2023-03-27 08:48:58 -0300

    #18

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
+// Configuração do jogo
 import config from "./config.js";
+//
+//Importar códigos das cenas
+//Cena de abertura
+import CenaDeAbertura from "./cena.js";
+// Cena principal
+import principal from "./principal.js"
+// Cena de encerramento
+import encerramento from "./encerramento.js"
 
 class Game extends Phaser.Game {
   constructor() {
     super(config);
+    //
+    // Carreagar as cenas
+    this.scene.add("abertura", abertura);
+    this.scene.add("principal", principal);
+    this.scene.add("encerramento", encerramento);
+    //
+    // Inciar pela cena de abertura
+    this.scene.start("abertura");
   }
 }
 
