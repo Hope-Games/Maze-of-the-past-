ded35b6826fbe018f33015d296537df8758d2398
Leticia Furlan <leticia.f14@aluno.ifsc.edu.br>
2023-06-19 12:24:09 +0000

    imagem final do jogo

--- cliente/js/cena-fim-do-jogo.js
+++ cliente/js/cena-fim-do-jogo.js
@@ -1,16 +1,20 @@
 export default class fim_do_jogo extends Phaser.Scene {
-    constructor() {
-      super("fim-do-jogo");
-    }
+  constructor() {
+    super("fim-do-jogo");
+  }
   
   preload() {
-      //this.load.image()
-    }
+    this.load.image("frase-final", "./assets/final/final.png");
+  }
   
   create() {
-      //this.add.image()
-    }
-  
-    upload() {}
+    this.imagem = this.add
+      .image(400, 225, "frase-final")
+      .setInteractive()
+      .on("pointerdown", () => {
+        this.imagem.destroy();
+        this.game.scene.start("abertura");
+      });
   }
+}
   
